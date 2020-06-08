5d56d73547435a850c2c5339f1083173c63ea71e
unknown <mattstokes94@yahoo.co.ukgit config --global user.name Stokesy94>
2020-06-08 19:07:57 +0100

    commit

--- src/Components/Page2.js
+++ src/Components/Page2.js
@@ -1,86 +1,168 @@
-import React, { Component } from "react";
-import "./Page2.css";
+import React, { Component, useRef } from "react";
+import "./Page2.scss";
 import logo from "../assets/logo.png";
 import Quiz from "../assets/Quiz.jpg";
 import Calc from "../assets/calc.jpg";
 import Mac from "../assets/mac.jpg";
-import Google from "../assets/google.jpg";
-import { Button } from "react-bootstrap";
+import me from "../assets/me.jpg";
+import CV from "../assets/CV.pdf";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faFile} from "@fortawesome/fontawesome-free-solid";
+import {faGithub, faLinkedin} from "@fortawesome/free-brands-svg-icons";
+import Compound from "../assets/compound.jpg";
+import { useIntersection} from "react-use"
+import gsap from "gsap";
 
-export default class Page2 extends Component {
-    render() {
-            return (
 
-                <div className="about">
+const Page2 = () => {
+    const sectionRef = useRef(null);
 
-                    <div>
-                        <a id="about" href="#projects #class #main1 #animateArrow "></a>
-                    </div>
+    const intersection = useIntersection(sectionRef, {
+        root: document.querySelector("main1"),
+        rootMargin: "-100px",
+        threshold: 1,
+    });
 
-                    <div className="b1">
-                        <h1 className="b1a">About Me</h1><br/>
-                        <p className="b1b">I am proficient at HTML, CSS and JavaScript and have a good understanding of various
-                            different frameworks and tools used in the modern workplace. I am actively searching for front end opportunities and outside of work and development I spend the weekends with my other half, usually finding a
-                            new restaurant or researching a new destination to travel too!</p>
-                    </div>
+    const fadeIn = (element) => {
+        gsap.to(element, 1, {
+            opacity: 0,
+            y: -20,
+            ease: "power4.out",
+            stagger: {
+                amount: .3
+            }
+        })
+    };
 
-                    <div className="b2">
-                        <h1 className="b2h1">Portfolio</h1>
-                        <div className="b2a">
-                            <img src={Quiz} className="pic"/>
-                            <h3>Quiz App</h3>
-                            <a href="https://github.com/stokesy94/quiz-application" target="_blank"><button className="buttons">View Code</button></a>
-                            <a href="https://stokesy94.github.io/quiz-application/" target="_blank"><button className="buttons">View Live</button></a>
-                            <p>Create, Save, Load and Play a Quiz!</p>
-                            <p>Challenges I Faced:</p>
-                            <ul>
-                                <li>Learning new technologies! XMLHttpRequests and JSON... I had not worked with either of these before and so researching, implementing and debugging took a lot longer than anticipated! </li>
-                                <li>CORS - Figuring out how to disable this to allow cross-origin access to my local server.  </li>
-                            </ul>
-                            <p>Solutions: <br/> Just keep trying different solutions found on StackOverflow and the likes.</p>
-                        </div>
-                        <div className="b2b">
-                            <img src={Calc} className="pic"/>
-                            <h3>Simple Calculator</h3>
-                            <a href="https://github.com/stokesy94/calculator" target="_blank"><button className="buttons">View Code</button></a>
-                            <a href="https://stokesy94.github.io/calculator/" target="_blank"><button className="buttons">View Live</button></a>
-                            <p>Simple Division, Multiplication, Addition and Subtraction</p>
-                            <p>Challenges I Faced:</p>
-                            <ul>
-                                <li>Figuring out the best and most efficient way of writing the code</li>
-                            </ul>
-                        </div>
-                        <div className="b2c">
-                            <img src={Mac} className="pic"/>
-                            <h3>Mac Recreation</h3>
-                            <a href="https://github.com/stokesy94/mac" target="_blank"><button className="buttons">View Code</button></a>
-                            <a href="https://stokesy94.github.io/mac/" target="_blank"><button className="buttons">View Live</button></a>
-                            <p>Improving design skills</p>
-                            <p>Challenges I Faced:</p>
-                            <ul>
-                                <li>Positioning</li>
-                            </ul>
-                            <p>Solutions: <br/> This was my first attempt at creating a website with multiple elements in and so figuring out how all elements interact with each other was a learning experience, achieved through many failures.</p>
-                        </div>
-                        <div className="b2d">
-                            <img src={Google} className="pic"/>
-                            <h3>Google Recreation</h3>
-                            <a href="https://github.com/stokesy94/google" target="_blank"><button className="buttons">View Code</button></a>
-                            <a href="https://stokesy94.github.io/google/" target="_blank"><button className="buttons">View Live</button></a>
-                            <p>Improving design skills</p>
-                            <p>Challenges I Faced:</p>
-                            <ul>
-                                <li>How to build a basic website</li>
-                            </ul>
-                            <p>Solutions: <br/> My first adventure into HTMl and CSS. This was an enjoyable experience and though it didn't take long, I learned a lot of the basic skills through this. </p>
-                        </div>
-                    </div>
+    const fadeOut = (element) => {
+        gsap.to(element, 1, {
+            opacity: 1,
+            y: -100,
+            ease: "power4.out",
 
-                    <a href="#contact" className="b1downicon"><img src={logo}/></a>
+        })
+    };
+
+
+
+    intersection && intersection.intersectionRatio < 1 ?
+        fadeOut(".b2a")
+        :
+        fadeIn(".b2a")
+
+    intersection && intersection.intersectionRatio < 1 ?
+        fadeOut(".b1element")
+        :
+        fadeIn(".b1element")
+
+    intersection && intersection.intersectionRatio < 1 ?
+        fadeOut(".b1b")
+        :
+        fadeIn(".b1b")
+
+
+
+
+
+    return (
+
+        <div className="about">
+
+            <div ref={sectionRef}>
+                <a id="about" href="#projects #class #main1 #animateArrow "></a>
+            </div>
+
+            <h1 className="b1a">About</h1><br/>
+
+            <div  className="b1">
+
+                <div className="b1element">
+                    <img src={me} className="pics" alt="A pic of me"/>
+                </div>
+
+                <div><p className="b1b">I have a passion for creating fast, responsive and dynamic websites. I am
+                    proficient at HTML, CSS and JavaScript and have a good understanding of various
+                    different frameworks and tools used in modern web design. </p>
+                </div>
+
+                <div className="b1element">
+                    <a href={CV} target="_blank"><FontAwesomeIcon icon={faFile} className="cv" size="5x"/></a>
+                </div>
+                <div className="b1element">
+                    <a href="https://www.linkedin.com/in/matthew-stokes-077414b7/" target="_blank"><FontAwesomeIcon
+                        icon={faLinkedin} className="linkedin" size="5x"/></a><br/>
+                </div>
+                <div className="b1element">
+                    <a href="https://github.com/stokesy94" target="_blank"><FontAwesomeIcon icon={faGithub} className="github" size="5x"/></a>
+                </div>
+
+            </div>
+
+            <h1 className="b2h1">Portfolio</h1>
+
+            <div className="b2">
+
+                <div className="b2a">
+                    <img src={Quiz} className="pic"/>
+                    <h3>Quiz App</h3>
+                    <a href="https://github.com/stokesy94/quiz-application" target="_blank">
+                        <button className="buttons">View Code</button>
+                    </a>
+                    <a href="https://stokesy94.github.io/quiz-application/" target="_blank">
+                        <button className="buttons">View Live</button>
+                    </a>
 
                 </div>
 
+                <div className="b2a">
+                    <img src={Compound} className="pic"/><h3>CI Calculator</h3>
+                    <a href="https://github.com/stokesy94/CompoundCalc" target="_blank">
+                        <button className="buttons">View Code</button>
+                    </a>
+                    <a href="https://stokesy94.github.io/CompoundCalc/" target="_blank">
+                        <button className="buttons">View Live</button>
+                    </a>
+                </div>
 
-            )
-        }
-    }
+
+                <div className="b2a">
+                    <img src={Calc} className="pic"/>
+                    <h3>Simple Calculator</h3>
+                    <a href="https://github.com/stokesy94/calculator" target="_blank">
+                        <button className="buttons">View Code</button>
+                    </a>
+                    <a href="https://stokesy94.github.io/calculator/" target="_blank">
+                        <button className="buttons">View Live</button>
+                    </a>
+
+                </div>
+                <div className="b2a">
+                    <img src={Mac} className="pic"/>
+                    <h3>Mac Recreation</h3>
+                    <a href="https://github.com/stokesy94/mac" target="_blank">
+                        <button className="buttons">View Code</button>
+                    </a>
+                    <a href="https://stokesy94.github.io/mac/" target="_blank">
+                        <button className="buttons">View Live</button>
+                    </a>
+
+                </div>
+
+            </div>
+
+            <div className="down">
+                <a id="animateArrow" href="#contact"><img src={logo} className="b1downicon" /></a>
+            </div>
+
+
+        </div>
+
+
+    )
+}
+
+export default Page2
+
+
+
+
--- src/Components/Page3.js
+++ src/Components/Page3.js
@@ -1,14 +1,80 @@
-import React, { Component } from "react";
-import "./Page3.css";
+import React, {useRef} from "react";
+import "./Page3.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub,  } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/fontawesome-free-solid";
+import gsap from "gsap";
+import {useIntersection} from "react-use";
 
-export default class Page3 extends Component {
-    render () {
+
+
+const Page3 = () => {
+
+    const sectionRef = useRef(null);
+
+    const intersection = useIntersection(sectionRef, {
+        root: document.querySelector("contactmoi"),
+        rootMargin: "0px",
+        threshold: .8,
+    });
+
+    const leftIn = (element) => {
+        gsap.to(element, 1, {
+            opacity: 1,
+            x: 0,
+            ease: "power4.out",
+            stagger: {
+                amount: .3
+            }
+        })
+    };
+
+    const leftOut = (element) => {
+        gsap.to(element, 1, {
+            opacity: 0,
+            x: -100,
+            ease: "power4.out",
+        })
+    };
+
+    const rightIn = (element) => {
+        gsap.to(element, 1, {
+            opacity: 1,
+            x: 0,
+            ease: "power4.out",
+            stagger: {
+                amount: .3
+            }
+        })
+    };
+
+    const rightOut = (element) => {
+        gsap.to(element, 1, {
+            opacity: 0,
+            x: 100,
+            ease: "power4.out",
+        })
+    };
+
+    intersection && intersection.intersectionRatio < .8 ?
+        leftOut(".contactmoi")
+        :
+        leftIn(".contactmoi")
+
+    intersection && intersection.intersectionRatio < .8 ?
+        leftOut(".contact-form")
+        :
+        leftIn(".contact-form")
+
+    intersection && intersection.intersectionRatio < .8 ?
+        rightOut(".contacticons")
+        :
+        rightIn(".contacticons")
+
+
         return (
 
-            <div className="contact">
+            <div ref={sectionRef} className="contact">
 
                 <a id="contact" href="#projects #about #main1"></a>
 
@@ -18,8 +84,6 @@ export default class Page3 extends Component {
 
 
                 <div className="contact-section">
-
-                    <div className="border"></div>
                     <form className="contact-form" action="index.html" method="post">
                         <input type="text" className="contact-form-text" placeholder="Your name" />
                         <input type="email" className="contact-form-text" placeholder="Your email" />
@@ -40,7 +104,6 @@ export default class Page3 extends Component {
                     </footer>
 
             </div>
-
         )
-    }
 }
+ export default Page3;
